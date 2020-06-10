acee2e9eb4816eb992897dc92fa90fd1c0c534e2
Tenzin Woeser <woeser@Tenzins-MacBook-Air.local>
2020-06-10 21:20:35 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req,res)=>{
+    res.send('API is running');
+})
+
+app.listen(PORT, ()=>{
+    console.log("Server started on port" +PORT)
+})
